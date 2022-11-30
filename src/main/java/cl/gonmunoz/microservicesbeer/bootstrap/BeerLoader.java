bb4825058f17b0e3111049bea734bbfd63b4c7b8
gonzalo.munoz <gonzaloan.munoz@gmail.com}>
2022-11-30 10:34:07 -0300

    Adds bootstrap

---
+++ src/main/java/cl/gonmunoz/microservicesbeer/bootstrap/BeerLoader.java
@@ -0,0 +1,47 @@
+package cl.gonmunoz.microservicesbeer.bootstrap;
+
+import cl.gonmunoz.microservicesbeer.domain.Beer;
+import cl.gonmunoz.microservicesbeer.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if(beerRepository.count() == 0){
+            beerRepository.save(Beer.builder()
+                    .beerName("Kunstmann")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Cuello Negro")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("10.95"))
+                    .build());
+
+        }
+        log.info("Loaded beers:{}", beerRepository.count());
+    }
+}
--- src/main/java/cl/gonmunoz/microservicesbeer/repositories/BeerRepository.java
+++ src/main/java/cl/gonmunoz/microservicesbeer/repositories/BeerRepository.java
@@ -1,9 +1,11 @@
 package cl.gonmunoz.microservicesbeer.repositories;
 
 import cl.gonmunoz.microservicesbeer.domain.Beer;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID>, CrudRepository<Beer, UUID> {
 }
